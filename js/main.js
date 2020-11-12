74e742ea078ea4fe3682efb48d1f7aed4c726a3f
onepro19 <w.settiawan@gmail.com>
2020-11-12 12:25:39 +0700

    revisi 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 if("serviceWorker" in navigator){
     navigator.serviceWorker
-    .register("/sw.js")
+    .register("./sw.js")
     .then(() => {
         console.log("Pendaftaran SW berhasil");
     })
