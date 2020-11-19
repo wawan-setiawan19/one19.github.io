84e481fae4314596f0311972f5e0ffa89efcb1c3
onepro19 <w.settiawan@gmail.com>
2020-11-19 19:51:52 +0700

    update ver

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ workbox.precaching.precacheAndRoute([
     {url:'/img/Fifa.svg', revision:'2'},
     {url:'/img/logo.png', revision:'2'},
     {url:'/js/api.js', revision:'2'},
-    {url:'/js/competitions.js', revision:'6'},
+    {url:'/js/competitions.js', revision:'7'},
     {url:'/js/db.js', revision:'2'},
     {url:'/js/idb.js', revision:'2'},
     {url:'/js/main.js', revision:'2'},
@@ -28,7 +28,7 @@ workbox.precaching.precacheAndRoute([
     {url:'/detail-competitions.html', revision:'2'},
     {url:'/manifest.json', revision:'2'},
     {url:'/push.js', revision:'2'},
-    {url:'/sw.js', revision:'7'}
+    {url:'/sw.js', revision:'8'}
 ]);
 
 workbox.routing.registerRoute(
